a1451a82083ed96693524563be2f88b0d1594d64
Victoria Sun <victoria.s.sun@gmail.com>
2017-09-01 16:16:10 -0700

    configure redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './redux/configureStores';
+
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
---
+++ src/redux/configureStores.js
@@ -0,0 +1,22 @@
+// @flow
+
+import { createStore, applyMiddleware, compose } from 'redux';
+import rootReducer from './rootReducer';
+
+const initialState = {};
+const enhancers = [];
+const middleware = [];
+
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension = window.devToolsExtension;
+
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
+
+const composedEnhancers = compose(applyMiddleware(...middleware), ...enhancers);
+
+const store = createStore(rootReducer, initialState, composedEnhancers);
+
+export default store;
