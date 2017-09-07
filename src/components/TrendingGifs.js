b91aa8406b12480e8eb44d80f35e03472568911b
Victoria Sun <victoria.s.sun@gmail.com>
2017-09-07 14:23:00 -0700

    remove

--- src/components/TrendingGifs.js
+++ src/components/TrendingGifs.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React, { PureComponent } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 
 import TrendingGif from '../containers/TrendingGif';
 
